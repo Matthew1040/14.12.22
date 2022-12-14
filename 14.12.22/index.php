88536ec16bddeb5ea99a7eee31dc59e308818c0d
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:20:55 -0300

    Aula1

---
+++ 14.12.22/index.php
@@ -0,0 +1,43 @@
+<?php
+$num1 = 100;
+$num2 = 200;
+
+$soma = $num1+$num2;
+ 
+echo "O primeiro número é:", $num1; 
+echo "<br/> O segundo número é:", $num2;
+echo "<br/> A soma dos dois números é:";
+echo $soma;
+
+if ($soma >= 200) {
+    echo "<br/> Resultado maior do que 200";
+} else {
+    echo "<br/> Resultado menor que 200";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
